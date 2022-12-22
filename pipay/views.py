566f56b96590c77acf8ccffd981c8a78f28e8a47
Nathaniel Egbodo <egbodonathaniel@yahoo.com>
2022-12-22 12:53:28 +0100

    #

--- pipay/views.py
+++ pipay/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from user.models import User
 
 # Create your views here.
 def index_view(request):
+    # the keyword "loc" of the dictionary below is to check if we are rendering the index page
     return render(request, 'pipay/home.html', {'loc': True,})
 
 def dashboard_view(request):
@@ -11,7 +13,10 @@ def task_view(request):
     return render(request, 'pipay/task.html')
 
 def couponVendor_view(request):
-    return render(request, 'pipay/couponVendor.html')
+    context = {
+        'user': User.objects.all(),
+    }
+    return render(request, 'pipay/couponVendor.html', context)
 
 def couponVerify_view(request):
     return render(request, 'pipay/couponVerify.html')
