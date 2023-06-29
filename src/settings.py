7c5bade7f703ccb09f2ca3357b839aad76f74712
Nathaniel Egbodo <egbodonathaniel@yahoo.com>
2023-06-30 00:39:55 +0100

    configured settings for mysql database

--- src/settings.py
+++ src/settings.py
@@ -95,13 +95,24 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ.get("NAME", "some_random_default_string"), # Username$database name
+            'USER': os.environ.get("USER", "some_random_default_string"), #Username:
+            'PASSWORD': os.environ.get("PASSWORD", "some_random_default_string"),
+            'HOST': os.environ.get("HOST", "some_random_default_string"), # Database host address           
+
+        }
+    }
 
 
 # Password validation
