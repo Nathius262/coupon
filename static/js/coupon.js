09f60134eb435d43c3525e76a406d83e027e731d
nathius262 <egbodonathaniel@yahoo.com>
2023-10-19 10:28:12 +0100

    '#'

--- static/js/coupon.js
+++ static/js/coupon.js
@@ -185,3 +185,31 @@ function toggleShowBalance(boolean){
 let footerDate = document.querySelector("#footer-date")
 let date = new Date
 footerDate.innerText = date.getFullYear()
+
+
+let y_top_objectEl = document.querySelectorAll('.scroll-y-top-el')
+let y_down_objectEl = document.querySelectorAll('.scroll-down-top-el')
+let x_top_objectEl = document.querySelectorAll('.scroll-x-top-el')
+let x_down_objectEl = document.querySelectorAll('.scroll-x-down-el')
+
+objectEl(y_top_objectEl, 'Y', '+')
+objectEl(y_down_objectEl, 'Y', '-')
+objectEl(x_top_objectEl, 'X', '+')
+objectEl(x_down_objectEl, 'X', '-')
+
+function objectEl(elementClass, axis, sign){
+    for (let element of elementClass){
+        let observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting){
+                    element.style.transform = `translate${axis}(0)`
+                }else{
+                    element.style.transform = `translate${axis}(${sign}50%)`
+                }
+            })
+        })
+        
+        observer.observe(element)
+    }
+    
+}
